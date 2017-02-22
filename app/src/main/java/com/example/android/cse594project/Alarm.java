9fa1f67c8fe7c139872e3da191f303ece0936ee4
Matt <mb92316@hotmail.com>
2017-02-22 03:36:49 -0800

    new committ

--- app/src/main/java/com/example/android/cse594project/Alarm.java
+++ app/src/main/java/com/example/android/cse594project/Alarm.java
@@ -2,7 +2,6 @@ package com.example.android.cse594project;
 
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
@@ -103,27 +102,24 @@ public class Alarm extends AppCompatActivity implements DatePickerDialog.OnDateS
         }
         long diff = d1.getTime() - date.getTime();
         System.out.println(diff);
-        String encryptedalarmNote = dbHandler.getNote(id);
-        String alarmNote = crypt.decrypt(encryptedalarmNote);
         if(choice == 1) {
-            scheduleNotification(getNotification(alarmNote), diff);
+            scheduleNoteNotification(diff);
         }
         else if(choice == 2) {
-            scheduleNotification(noteText, diff);
+            scheduleVoiceNotification(diff);
         }
         else{
             Toast.makeText(this, "something broke", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void scheduleNotification(Notification notification, long delay) {
+    private void scheduleNoteNotification(long delay) {
         editor = pref.edit();
         alarmID++;
         editor.putInt("AlarmID", alarmID);
         editor.commit();
         Intent notificationIntent = new Intent(this, NotificationPublisher.class);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 20);
-        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
         notificationIntent.putExtra("id", id);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), alarmID, notificationIntent, 0);
         AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
@@ -131,26 +127,16 @@ public class Alarm extends AppCompatActivity implements DatePickerDialog.OnDateS
     }
 
 
-    private void scheduleNotification(String notification, long delay) {
+    private void scheduleVoiceNotification(long delay) {
         editor = pref.edit();
         alarmID++;
         editor.putInt("AlarmID", alarmID);
         editor.commit();
         Intent notificationIntent = new Intent(this, AlarmPublisher.class);
         notificationIntent.putExtra(AlarmPublisher.NOTIFICATION_ID, 20);
-        notificationIntent.putExtra(AlarmPublisher.NOTIFICATION, notification);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                this.getApplicationContext(), alarmID, notificationIntent, 0);
+        notificationIntent.putExtra("id", id);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), alarmID, notificationIntent, 0);
         AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + delay, pendingIntent);
-    }
-
-
-    private Notification getNotification(String content) {
-        Notification.Builder builder = new Notification.Builder(this);
-        builder.setContentTitle("Scheduled Notification");
-        builder.setContentText(content);
-        builder.setSmallIcon(R.drawable.ic_launcher);
-        return builder.build();
     }
 }
