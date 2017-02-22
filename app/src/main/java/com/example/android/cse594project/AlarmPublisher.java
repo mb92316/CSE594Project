be4c1e7c2fc3b957403b83efb5745b428309af0b
Matt <mb92316@hotmail.com>
2017-02-22 03:40:11 -0800

    new committ

--- app/src/main/java/com/example/android/cse594project/AlarmPublisher.java
+++ app/src/main/java/com/example/android/cse594project/AlarmPublisher.java
@@ -4,34 +4,46 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 
-public class AlarmPublisher extends BroadcastReceiver {
+import java.util.Locale;
 
-    String note;
+public class AlarmPublisher extends BroadcastReceiver implements TextToSpeech.OnInitListener {
+
+    String noteText;
     private TextToSpeech tts;
     public static String NOTIFICATION_ID = "notification-id";
     public static String NOTIFICATION = "notification";
-    DBHandler dbHandler;
-    Crypt crypt;
-    Context mcontext;
-    int id;
+
     public void onReceive(Context context, Intent intent) {
-        mcontext = context;
-        //noteText = intent.getStringExtra(NOTIFICATION);
+        noteText = intent.getStringExtra(NOTIFICATION);
         Intent intent1 = new Intent(context, TTS.class);
-        id = intent.getIntExtra("id", 0);
-        getNote();
-        if(note != null) {
-            note = crypt.decrypt(note);
-            intent1.putExtra(NOTIFICATION, note);
-            context.startService(intent1);
+        intent1.putExtra(NOTIFICATION, noteText);
+        context.startService(intent1);
+    }
+
+    private void speakOut(String text) {
+        String utteranceId=this.hashCode() + "";
+        //Get the text typed
+        //If no text is typed, tts will read out 'You haven't typed text'
+        //else it reads out the text you typed
+        if (text.length() == 0) {
+            tts.speak("No text typed", TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
         }
     }
 
-    public void getNote() {
-        dbHandler = new DBHandler(mcontext, null, null, 1);
-        note = dbHandler.getNote(id);
-    }
 
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
 }
 
